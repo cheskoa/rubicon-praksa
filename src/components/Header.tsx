04478857c6bf6f327f64c059cff95b62aa430c4b
cheskoa <ceskoa01@gmail.com>
2025-09-17 09:34:23 +0200

    header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import "../styles/header.css";
 export default function Header() {
   return (
     <header className="header">
-      <div className="logo">BitAlijansa Internship</div>
+      <div className="logo">BAIR</div>
       <div className="search-bar">
         <input type="text" placeholder="Search movies or series..." />
       </div>
